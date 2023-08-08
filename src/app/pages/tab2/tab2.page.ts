805dd66f3187f192915f911405ffe9118bdc1590
amarianjel <abrm.engineer@gmail.com>
2023-08-08 18:10:41 -0400

    ya toma el qr y lo guarda

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,51 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+// import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
-import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnDestroy{
 
-  constructor( private alertController: AlertController){}
-
-  async startScanner(){
-    const result = await BarcodeScanner.startScan();
-    console.log("~file: tab2.page.ts ~ line 16 ~ Tab2 ~ startScanner ~ result", result)
-  }
-
-  async checkPermission(){
-    return new Promise(async ( resolve, reject ) => {
-      const status = await BarcodeScanner.checkPermission({ force: true });
-      
-      if( status.granted ){
-        resolve( true );
-      }else if( status.denied ){
-        const alert = await this.alertController.create({
-          header: 'No hay permiso',
-          message:'Debes darle el permiso para usar la cámara.',
-          buttons:[{
-            text: 'No',
-            role:"cancel"
-          },{
-            text: 'Abrir Configuracion',
-            handler: () => {
-              resolve(false);
-              BarcodeScanner.openAppSettings();
-            }
-          }]
-        });
-
-        await alert.present();
-      }else{
-        resolve(false);
+    // https://www.npmjs.com/package/angularx-qrcode
+    qrCodeString = 'This is a secret qr code message';
+    scannedResult: any;
+    content_visibility = '';
+  
+    constructor(
+      // private barcodeScanner: BarcodeScanner
+      ) {}
+  
+    // startScan() {
+    //   this.barcodeScanner.scan().then(barcodeData => {
+    //     console.log('Barcode data', barcodeData);
+    //     this.scannedResult = barcodeData?.text;
+    //    }).catch(err => {
+    //        console.log('Error', err);
+    //    });
+    // }
+  
+    async checkPermission(): Promise<boolean | any> {
+      try {
+        // check or request permission
+        const status = await BarcodeScanner.checkPermission({ force: true });
+        if (status.granted) {
+          // the user granted permission
+          return true;
+        }
+        return false;
+      } catch(e) {
+        console.log(e);
       }
-    })
-
-
-
-  }
-
+    }
+  
+    async startScan() {
+      try {
+        const permission = await this.checkPermission();
+        if(!permission) {
+          return;
+        }
+        await BarcodeScanner.hideBackground();
+        document.querySelector('body')?.classList.add('scanner-active');
+        this.content_visibility = 'hidden';
+        const result = await BarcodeScanner.startScan();
+        console.log(result);
+        BarcodeScanner.showBackground();
+        document.querySelector('body')?.classList.remove('scanner-active');
+        this.content_visibility = '';
+        if(result?.hasContent) {
+          this.scannedResult = result.content;
+          console.log(this.scannedResult);
+        }
+      } catch(e) {
+        console.log(e);
+        this.stopScan();
+      }
+    }
+  
+    stopScan() {
+      BarcodeScanner.showBackground();
+      BarcodeScanner.stopScan();
+      document.querySelector('body')?.classList.remove('scanner-active');
+      this.content_visibility = '';
+    }
+  
+    ngOnDestroy(): void {
+        this.stopScan();
+    }
 }
