4235ab39fb6fad8014bdd2bcddead74adcf57c07
amarianjel <abrm.engineer@gmail.com>
2023-08-08 10:53:22 -0400

    Agregando los codigos para el uso de la camara, en tab 1 capacitor, en tab 2 cordova

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { BarcodeScanner, SupportedFormat } from '@capacitor-community/barcode-scanner';
+// import { BarcodeScanner, SupportedFormat } from '@capacitor-community/barcode-scanner';
 import { AlertController, ToastController } from '@ionic/angular';
 import { DataLocalService } from 'src/app/services/data-local.service';
 
@@ -17,67 +17,64 @@ export class Tab1Page {
   };
 
   public inProcess: boolean = false;
- 
+
   constructor( private alertCtrl: AlertController, private toastCtrl: ToastController ) { }
- 
+
   ionViewWillLeave() {
     console.log('ionViewWillLeave ');
     window.document.querySelector('body')?.classList.remove('scanner-active');
- 
-  }
- 
-  ionViewDidLeave() {
-    console.log('ionViewDidLeave');
-    BarcodeScanner.stopScan();
- 
-  }
- 
-  async scan() {
- 
-    this.inProcess = true;
-    // Este es lo importante!
-    window.document.querySelector('body')?.classList.add('scanner-active');
- 
-    // Check camera permission
-    // This is just a simple example, check out the better checks below
-    await BarcodeScanner.checkPermission({ force: true });
-    // make background of WebView transparent
-    // note: if you are using ionic this might not be enough, check below
-    BarcodeScanner.hideBackground();
-    const result = await BarcodeScanner.startScan({ targetedFormats: [SupportedFormat.QR_CODE] }); // start scanning and wait for a result
-    // if the result has content
-    if (result.hasContent) {
-      console.log(result.content); // log the raw scanned content
-      // this.stopScan()
-      this.presentAlert();
-    }
   }
 
-  stopScan(){
-    BarcodeScanner.showBackground();
-    BarcodeScanner.stopScan();
-  };
- 
-  async presentAlert() {
-    const alert = await this.alertCtrl.create({
-      header: '¡Excelente!',
-      // subHeader: 'La asistencia se registró correctamente',
-      message: 'La asistencia se guardó correctamente',
-      buttons: [
-        {
-          text: 'OK',
-          role: 'confirm',
-          handler: () => {
-            console.log('Alert confirmed');
-            this.inProcess = false;
+  // ionViewDidLeave() {
+  //   console.log('ionViewDidLeave');
+  //   BarcodeScanner.stopScan();
+  // }
+
+  // async scan() {
+  //   this.inProcess = true;
+  //   // Este es lo importante!
+  //   window.document.querySelector('body')?.classList.add('scanner-active');
+
+  //   // Check camera permission
+  //   // This is just a simple example, check out the better checks below
+  //   await BarcodeScanner.checkPermission({ force: true });
+  //   // make background of WebView transparent
+  //   // note: if you are using ionic this might not be enough, check below
+  //   BarcodeScanner.hideBackground();
+  //   const result = await BarcodeScanner.startScan({ targetedFormats: [SupportedFormat.QR_CODE] }); // start scanning and wait for a result
+  //   // if the result has content
+  //   if (result.hasContent) {
+  //     console.log(result.content); // log the raw scanned content
+  //     // this.stopScan()
+  //     this.presentAlert();
+  //   }
+  // }
+
+  // stopScan(){
+  //   BarcodeScanner.showBackground();
+  //   BarcodeScanner.stopScan();
+  // };
+
+  // async presentAlert() {
+  //   const alert = await this.alertCtrl.create({
+  //     header: '¡Excelente!',
+  //     // subHeader: 'La asistencia se registró correctamente',
+  //     message: 'La asistencia se guardó correctamente',
+  //     buttons: [
+  //       {
+  //         text: 'OK',
+  //         role: 'confirm',
+  //         handler: () => {
+  //           console.log('Alert confirmed');
+  //           this.inProcess = false;
             
-          },
-        },
-      ],
-      mode: 'ios',
-      translucent: true,
-      animated: true
-    });
-    await alert.present();
-  }
+  //         },
+  //       },
+  //     ],
+  //     mode: 'ios',
+  //     translucent: true,
+  //     animated: true
+  //   });
+  //   await alert.present();
+  // }
 }
--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx'; 
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(private barcodeScanner: BarcodeScanner) {}
+
+  scan() {
+    this.barcodeScanner.scan().then((barcodeData) => {
+      console.log('Resultado del escaneo:', barcodeData.text);
+    }).catch((err) => {
+      console.error('Error en el escaneo:', err);
+    });
+  }
+
+  // scan(){
+  //   this.barcodeScanner.scan().then( barcodeScanner => {
+  //     console.log("Barcode data", this.barcodeScanner);
+  //   }).catch( err => {
+  //     console.log('Error ', err);
+  //   })
+  // }
 
 }
